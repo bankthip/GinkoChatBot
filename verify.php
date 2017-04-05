c0b61a3b767cb19da5b15b8252f45a971e63de66
bankthip <bankthip@gmail.com>
2017-04-05 23:58:36 +0700

    Add files via upload

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'W6KJ9hOZxmYxq166VcKrIH13UEGwuKQzWpU21N8uKqXXXRf3Ot6ukCA/ofBVZ9pp3iek8k+dFc7PF2xdV8Mq7Eo8wh0pO4JWI+focx4yF2Uo1vQ8yVW9l3nNGStGWluAGO7GL/LxWHhTpu3B6feCfQdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
