53c746be6941e7b92f8b0c28cc8e94d5f81391f4
bankthip <bankthip@gmail.com>
2017-04-06 00:15:06 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -18,7 +18,10 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $ดีจ๊ะ
+				'text' => 'bank'
+				
+				
+				
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
