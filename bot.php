7737fa03fcad099700fcc3dfd9216166c1cd4554
bankthip <bankthip@gmail.com>
2017-04-06 23:09:05 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,6 @@
 <?php
 $access_token = 'W6KJ9hOZxmYxq166VcKrIH13UEGwuKQzWpU21N8uKqXXXRf3Ot6ukCA/ofBVZ9pp3iek8k+dFc7PF2xdV8Mq7Eo8wh0pO4JWI+focx4yF2Uo1vQ8yVW9l3nNGStGWluAGO7GL/LxWHhTpu3B6feCfQdB04t89/1O/w1cDnyilFU=';
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -9,34 +10,17 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if (
-			== 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			if ($text == "bank"){
-				$messages = [
+			$messages = [
 				'type' => 'text',
-				'text' => 'thank'
-				];	
-				
-			}else{
-				$messages = [
-					'type' => 'text',
-					'text' => $text
-//				    		'type'=> 'image',
-//   		'originalContentUrl'=> 'http://ginkotown.com/line_text/1024.jpg',
-//  		'previewImageUrl'=> 'http://ginkotown.com/line_text/240.jpg'
-				];	
-			}
-			
-				
-			
-				
-			
+				'text' => $text
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -44,8 +28,6 @@ if (!is_null($events['events'])) {
 				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
-			
-			
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
